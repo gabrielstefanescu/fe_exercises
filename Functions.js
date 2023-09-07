975a88e18b4ca48f192bf3fee2156a58608f2e95
yuliy_barzov <yuliy.barzov@serviceware.de>
2023-09-07 16:03:03 +0300

    Second commit

--- Functions.js
+++ Functions.js
@@ -1,28 +1,147 @@
+// 4. Use functions to repeat actions.
+
+//Exercise 1: Function Declaration
 //Write a function called greet that takes a name as an argument and returns a greeting message.
 //For example, if the name is "Alice," the function should return "Hello, Alice!"
 
 function greet(personName) {
   return `Hello, ${personName}!`;
 }
-// let personName = "Alice";
 console.log(greet("Alice"));
 
-carName = "Saab";
+// Exercise 2: Function Expression
+// Create a function expression called multiply that takes two numbers as arguments and returns their product.
+// Call the function with different sets of numbers and log the results to the console.
 
-function myFunction() {
-  let carName = "Volvo";
-  // code here CAN use carName
-  return carName;
+function multiply(a, b) {
+  return a * b;
 }
-//carName = "Saab";
-console.log(carName);
-//console.log(myFunction());
+console.log(multiply(5, 10));
 
-console.log(myFunction(5));
-function myFunction(y) {
-  return y * y;
+// Exercise 3: Function with Default Parameter
+// Write a function called calculateArea that calculates the area of a rectangle.
+// The function should take the length and width as arguments, with a default width of 5 units. Log the area to the console.
+
+function calculateArea(length, width = 5) {
+  return length * width;
+}
+console.log(calculateArea(11));
+
+// Exercise 4: Function with Variable Number of Arguments
+// Create a function called sum that takes any number of arguments and returns their sum.
+//  You can use the arguments object to access the arguments inside the function.
+
+//solution 1
+function sum(number1, number2) {
+  return number1 + number2;
+}
+console.log(sum(15, 20));
+
+//solution 2
+function sum() {
+  return arguments[0] + arguments[1];
+}
+console.log(sum(15, 20));
+
+// Exercise 5: Recursive Function
+// Write a recursive function called factorial that calculates the factorial of a positive integer.
+// The factorial of a number n is the product of all positive integers from 1 to n.
+// For example, factorial(5) should return 120.
+
+//Solution 1 - Factorial without recursion
+// function factorial(n) {
+//   let result = n;
+//   while (n > 1) {
+//     n--;
+//     result *= n;
+//   }
+//   return result;
+// }
+// console.log(factorial(5));
+
+//Solution 2 - Factorial with recursion
+function factorial(x) {
+  if (x == 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+let num = 5;
+if (num >= 0) {
+  let result = factorial(num);
+  console.log(result);
 }
 
-const x = (x, y) => x * y;
-console.log(x(6, 5));
+// 5. Break down tasks and customize functions with parameters
 
+// Exercise 1: Calculate the Area of a Rectangle
+
+// Write a function called calculateRectangleArea that takes the length and width of a rectangle as parameters and returns the area.
+//  Call the function with different sets of length and width values.
+function calculateRectangleArea(width, length) {
+  for (let i = 0; i < width.length; i++) {
+    area.push(width[i] * length[i]);
+  }
+  return area;
+}
+let area = [];
+console.log(calculateRectangleArea([4, 5, 6], [11, 12, 13]));
+
+// Exercise 2: Find the Maximum Value in an Array
+
+// Create a function called findMax that takes an array of numbers as a parameter and returns the maximum value in the array.
+//  Test the function with various arrays.
+
+function findMax(array) {
+  array.sort(function (a, b) {
+    return a - b;
+  });
+  return array;
+}
+console.log(findMax([60, 20, 50, 100, 40, 10, 30, 70]));
+
+// Exercise 3: Convert Temperature Units
+
+// Write a function called convertTemperature that converts a temperature from Celsius to Fahrenheit.
+//  The function should take the temperature in Celsius as a parameter and return the temperature in Fahrenheit.
+//   Test the function with different temperatures.
+
+function convertTemperature(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+console.log(convertTemperature(20));
+
+// Exercise 4: Calculate the Factorial of a Number
+
+// Create a function called calculateFactorial that calculates the factorial of a positive integer.
+// The function should take the integer as a parameter and return the factorial. Test the function with different integers.
+function factorial(n) {
+  let result = n;
+  while (n > 1) {
+    n--;
+    result *= n;
+  }
+  return result;
+}
+console.log(factorial(6));
+
+// Exercise 5: Generate a Random Password
+
+// Write a function called generatePassword that generates a random password of a specified length.
+// The function should take the password length as a parameter and return the generated password.
+// Customize the characters used in the password generation (e.g., letters, numbers, symbols)
+// and test the function with different password lengths.
+
+function generatePassword(length) {
+  let pass = "";
+  let str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+  for (let i = 1; i <= length; i++) {
+    let char = Math.floor(Math.random() * str.length + 1);
+    pass += str.charAt(char);
+  }
+  return pass;
+}
+console.log(generatePassword(10));
--- Variables.js
+++ Variables.js
@@ -1,7 +1,15 @@
+// Exercise 1: Variable Declaration
+// Declare three variables (firstName, lastName, and age) and assign your name and age to them.
+//  Then, log a message to the console that displays your name and age.
+
 let firstName = "John";
 let lastName = "Doe";
 let age = 35;
 console.log(`${firstName} ${lastName} ${age}`);
+
+// Exercise 2: Variable Types
+// Create variables to store values of different data types: a number, a string, a boolean, an array, and an object.
+// Log the types of these variables to the console.
 
 let typeNumber = 20;
 let typeString = "String";
@@ -20,16 +28,28 @@ console.log(
     JSON.stringify(typeObject)
 );
 
+// Exercise 3: Variable Manipulation
+// Declare a variable counter and set its initial value to 0.
+// Then, increment it by 1, decrement it by 1, and multiply it by 2. Log the final value of counter to the console.
 let counter = 0;
 counter++;
 counter--;
 counter *= 2;
 console.log(counter);
 
+// Exercise 4: String Concatenation
+// Create two variables, firstName and lastName, with your first and last names.
+// Use string concatenation to create a full name and log it to the console.
+
 let firstName2 = "John";
 let lastName2 = "Doe";
-console.log(firstName2+" "+lastName2);
+console.log(firstName2 + " " + lastName2);
+
+// Exercise 5: Temperature Conversion
+// Declare a variable celsius and set it to a temperature in Celsius.
+// Convert this temperature to Fahrenheit using the formula Fahrenheit = (Celsius * 9/5) + 32.
+// Log the temperature in both Celsius and Fahrenheit to the console.
 
 let celsius = 30;
-let fahrenheit = (celsius * 9/5) + 32;
-console.log(celsius+"\n"+fahrenheit);
+let fahrenheit = (celsius * 9) / 5 + 32;
+console.log(celsius + "\n" + fahrenheit);
