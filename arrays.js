085803d2ab9a0ab8626359ee0fbfc04b692908f2
yuliy_barzov <yuliy.barzov@serviceware.de>
2023-09-20 16:36:44 +0300

    Arrays final commit

--- arrays.js
+++ arrays.js
@@ -34,20 +34,62 @@ console.log(x2);
 // Task 2: Reverse an Array
 // Create a function that reverses the elements of an array without using the built-in reverse method.
 // The original array should remain unchanged.
+
+// //Solution1:
+// function reverseArray(array) {
+//   let reverseElements = "";
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     reverseElements += array[i];
+//   }
+//   return reverseElements;
+// }
+// let x3 = reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// console.log(x3);
+
+// //Solution2:
+// function reverseArray(array) {
+//   let reverseElements = [];
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     let element = array[i];
+//     reverseElements.push(element);
+//   }
+//   return reverseElements;
+// }
+// let x4 = reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// console.log(x4);
+
+//Solution3 with increment operator and string:
+// function reverseArray(array) {
+//   for (let element of array) {
+//     if (typeof element !== "number") {
+//       return ("Invalid input");
+//     }
+//   }
+//   let reverseElements = "";
+//   for (let i = 0; i < array.length; i++) {
+//     reverseElements += array.length - i + " ";
+//   }
+//   return reverseElements;
+// }
+// let x3 = reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// console.log(x3);
+
+//Solution4 with increment operator and array
 function reverseArray(array) {
-  for (let element of array) {
-    if (typeof element !== "number") {
-      return ("Invalid input");
+    for (let element of array) {
+      if (typeof element !== "number") {
+        return ("Invalid input");
+      }
     }
+    let reverseElements = [];
+    for (let i = 0; i < array.length; i++) {
+      reverseElements += array.length - i + " ";
+    }
+    return reverseElements;
   }
-  let reverseElements = "";
-  for (let i = 0; i < array.length; i++) {
-    reverseElements += array.length - i + " ";
-  }
-  return reverseElements;
-}
-let x3 = reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log(x3);
+  let x3 = reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  console.log(x3);
+
 
 // Task 3: Remove Duplicates
 // Write a function that removes duplicate values from an array and returns a new array with unique values.
@@ -67,6 +109,7 @@ console.log(x5);
 // Task 4: Flatten Nested Arrays
 // Implement a function that flattens a nested array into a single flat array.
 // For example, if the input is [1, [2, 3], [4, [5, 6]]], the output should be [1, 2, 3, 4, 5, 6].
+//Solution1
 function flattenArray(arr) {
   let flattenedArray = [];
   function helper(array) {
@@ -84,6 +127,20 @@ function flattenArray(arr) {
 }
 let flattened = flattenArray([1, 2, [3, 4, 5], [6, [7, 8], 9], 10]);
 console.log(flattened);
+
+//Solution2
+let flattenedArray = [];
+function helper(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      helper(array[i]);
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+}
+helper([1, 2, [3, 4, 5], [6, [7, 8], 9], 10]);
+console.log(flattenedArray);
 
 // Task 5: Array Sorting
 // Write a program that prompts the user to enter a list of numbers and then sorts and displays the numbers in ascending order 
